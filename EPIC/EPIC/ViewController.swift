ecbe7ad5c00e5fedb67e7722f028a080fef5f6b1
LopezsF <91020160+LopezsF@users.noreply.github.com>
2022-06-14 16:01:23 -0300

    Create the EPIC project.

---
+++ EPIC/EPIC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EPIC
+//
+//  Created by Francisco Lopez on 14/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
