9565b06f40f6b40137a8b6b9205aa6e1a16da572
Francisco Lopez <lopezeoane.f@gmail.com>
2022-06-14 21:16:08 -0300

    add pod

---
+++ EPIC/Pods/Target Support Files/Pods-EPIC/Pods-EPIC-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EPIC : NSObject
+@end
+@implementation PodsDummy_Pods_EPIC
+@end
---
+++ EPIC/Pods/Target Support Files/Pods-EPICTests/Pods-EPICTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EPICTests : NSObject
+@end
+@implementation PodsDummy_Pods_EPICTests
+@end
